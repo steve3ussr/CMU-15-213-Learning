10fc9a5fc953ae8194accea9fdd5e6d0d3efc8a6
steve3ussr <2816825048@qq.com>
2025-03-31 23:09:32 +0800

    pass on 1-2

--- Labs/ShLab/tsh.c
+++ Labs/ShLab/tsh.c
@@ -165,6 +165,30 @@ int main(int argc, char **argv)
 */
 void eval(char *cmdline) 
 {
+    char *argv[MAXARGS];
+    char buf[MAXLINE];
+    int bg;
+    pid_t pid;
+
+    strcpy(buf, cmdline);
+    bg = parseline(buf, argv);
+
+    if(argv[0] == NULL) return;
+
+    if(!builtin_cmd(argv)){
+        if((pid=fork()) == 0){
+            if(execve(argv[0], argv, environ) < 0){
+                printf("%s: Bad command\n", argv[0]);
+                exit(0);
+            }
+        }
+    }
+
+
+
+
+
+
     return;
 }
 
@@ -231,6 +255,10 @@ int parseline(const char *cmdline, char **argv)
  */
 int builtin_cmd(char **argv) 
 {
+    if(strcmp(argv[0], "quit") == 0) {exit(0); return 1;}
+    if(strcmp(argv[0], "jobs") == 0) { listjobs(jobs); return 1;}
+    if(strcmp(argv[0], "bg") == 0) { do_bgfg(argv); return 1;}
+    if(strcmp(argv[0], "fg") == 0) { do_bgfg(argv); return 1;}
     return 0;     /* not a builtin command */
 }
 
