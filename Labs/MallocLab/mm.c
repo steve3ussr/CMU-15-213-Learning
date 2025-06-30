c8c626c7226bcb0b6fac71b27eac7eebaab0a43b
steve3ussr <2816825048@qq.com>
2025-06-30 21:39:25 +0800

    v1: implicit, no opts, not checked

--- Labs/MallocLab/mm.c
+++ Labs/MallocLab/mm.c
@@ -38,18 +38,102 @@ team_t team = {
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
 
+
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+/* self defined macros */
+#define WSIZE               4
+#define DSIZE               8
+#define CHUNKSIZE           (1<<12)  // 4K
+#define MAX(x, y)           ((x)>(y)? (x) : (y))
+#define PACK(size, alloc)   ((size) | (alloc))
+#define GET(p)              (*(unsigned int *)(p))
+#define PUT(p, val)         (*(unsigned int *)(p) = (val))
+#define GET_SIZE(p)         (GET(p) & (~0x7))
+#define GET_ALLOC(p)        (GET(p) & (0x1))
+#define GET_PREV_ALLOC(p)   (GET(p) & (0x2))
+#define HDRP(bp)            ((char *)(bp) - WSIZE)
+#define FTRP(bp)            ((char *)(bp) + GET_SIZE(HDBP(bp)) - WSIZE)
+#define NEXT_BLKP(bp)       ((char *)(bp) + GET_SIZE(HDRP(bp)))
+#define PREV_BLKP(bp)       ((char *)(bp) - GET_SIZE(HDRP(bp) - WSIZE))
+
+/* self defined vars */
+void * heap_listp;  // the init bp
+
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    void* tmp;
+    if ((tmp = mem_sbrk(4*WSIZE)) == (void *)-1)
+        return -1;
+
+    PUT(tmp, 0);
+    PUT(tmp+1*WSIZE, PACK(DSIZE, 1));
+    PUT(tmp+2*WSIZE, PACK(DSIZE, 1));
+    PUT(tmp+3*WSIZE, PACK(0    , 1));
+    heap_listp = tmp+2*WSIZE;
+
+    if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
+        return -1;
     return 0;
+}
+
+static void * extend_heap(size_t words)
+{
+    size_t real_words = (words%2) ? (words+1) : words;
+    size_t real_bytes = real_words * WSIZE;
+
+    void* bp;
+    if ((bp = mem_sbrk(real_bytes)) == -1)
+        return NULL;
+
+    PUT(HDRP(bp), PACK(real_bytes, 0));
+    PUT(FTRP(bp), PACK(real_bytes, 0));
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+
+    return coalesce(bp);
+}
+
+static void * coalesce(void * bp)
+{
+    size_t prev_alloc_status = GET_ALLOC(HDRP(PREV_BLKP(bp)));  // 0-free, 1-alloced
+    size_t prev_size         = GET_SIZE(HDRP(PREV_BLKP(bp)));
+    size_t next_alloc_status = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t next_size         = GET_SIZE(HDRP(NEXT_BLKP(bp)));
+    size_t curr_size         = GET_SIZE(HDRP(bp));
+
+    if (prev_alloc_status && next_alloc_status){
+    }
+
+    // prev is free, next is alloced
+    else if (!prev_alloc_status && next_alloc_status){
+        size_t new_size = curr_size + prev_size;
+        PUT(HDRP(PREV_BLKP(bp)), PACK(new_size, 0));
+        PUT(FTRP(bp), PACK(new_size, 0));
+        bp = PREV_BLKP(bp);
+    }
+
+    // prev is alloced, next is free
+    else if (prev_alloc_status && !next_alloc_status){
+        size_t new_size = curr_size + next_size;
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(new_size, 0));
+        PUT(HDRP(bp), PACK(new_size, 0));
+    }
+
+    // prev is free, next is free
+    else{
+        size_t new_size = curr_size + next_size + prev_size;
+        PUT(HDRP(PREV_BLKP(bp)), PACK(new_size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(new_size, 0));
+    }
+
+    return bp;
 }
 
 /* 
@@ -58,21 +142,80 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+    // int newsize = ALIGN(size + SIZE_T_SIZE);
+    // void *p = mem_sbrk(newsize);
+    // if (p == (void *)-1)
+	// return NULL;
+    // else {
+    //     *(size_t *)p = size;
+    //     return (void *)((char *)p + SIZE_T_SIZE);
+    // }
+    if (size == 0)
+        return NULL;
+
+    size_t aligned_size = ALIGN(size) + DSIZE;  // full block size
+    size_t extend_size = MAX(aligned_size, CHUNKSIZE);
+
+    // search
+    void * bp;
+    if ((bp = find_fit(aligned_size)) != NULL){
+        place(bp, aligned_size);
+        return bp;
     }
+
+    if ((bp = extend_heap(extend_size/WSIZE)) == NULL)
+        return NULL;
+    place(bp, aligned_size);
+    return bp;
+
 }
+
+static void * find_fit(size_t size)
+{
+    void* tmp = heap_listp;
+
+    for (void* tmp=heap_listp; GET(tmp)!=PACK(0, 1); tmp=NEXT_BLKP(tmp)) {
+        
+        if (!GET_ALLOC(HDRP(tmp)) && GET_SIZE(HDRP(tmp)) >= size)
+            return tmp;
+    }
+
+    // tmp now points to epilogue block
+    return NULL;
+}
+
+
+static void place(void * bp, size_t size)
+{
+    size_t curr_size = GET_SIZE(HDRP(bp));
+
+    // no need to split
+    if (curr_size - size < 2*DSIZE) {
+        PUT(HDRP(bp), PACK(curr_size, 1));
+        PUT(FTRP(bp), PACK(curr_size, 1));
+        return; 
+    }
+    
+
+    // split the block
+    PUT(HDRP(bp), PACK(size, 1));
+    PUT(FTRP(bp), PACK(size, 1));
+
+    bp = NEXT_BLKP(bp);
+    PUT(HDRP(bp), PACK(curr_size-size, 0));
+    PUT(FTRP(bp), PACK(curr_size-size, 0));
+    coalesce(bp);
+}
+
 
 /*
  * mm_free - Freeing a block does nothing.
  */
 void mm_free(void *ptr)
 {
+    PUT(HDRP(ptr), PACK(GET_SIZE(HDRP(ptr)), 0));
+    PUT(FTRP(ptr), PACK(GET_SIZE(FTRP(ptr)), 0));
+    coalesce(ptr);
 }
 
 /*
@@ -80,6 +223,13 @@ void mm_free(void *ptr)
  */
 void *mm_realloc(void *ptr, size_t size)
 {
+    if (ptr == NULL){
+        return mm_malloc(size);
+    }
+    if (size == 0){
+        mm_free(ptr);
+    }
+
     void *oldptr = ptr;
     void *newptr;
     size_t copySize;
@@ -87,6 +237,7 @@ void *mm_realloc(void *ptr, size_t size)
     newptr = mm_malloc(size);
     if (newptr == NULL)
       return NULL;
+
     copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
     if (size < copySize)
       copySize = size;
--- Labs/MallocLab/note_malloclab.md
+++ Labs/MallocLab/note_malloclab.md
@@ -7,6 +7,8 @@
 
 ## Working On
 
+
+
 ### memlib.c
 
 学生将使用这个库里的函数, 而不是系统调用
@@ -19,11 +21,73 @@
 - mem_heapsize: 实际使用的堆内存大小
 - mem_pagesize: 返回系统的pagesize
 
+
+
+### macros in mm.c
+
+- [x] GET PUT是否一定要每次操作4B啊
+
+- Word Size, Double Word, Chunk (4K)
+- **PACK**, 将29 bits的size (后3 bit为隐式的8B对齐, 全0意味着必须是8的倍数) 和3bits的 alloc status打包
+- GET PUT, 强转指针为WordSize指针(4 Bytes), 一次操作4 Bytes
+- GET_SIZE, mask掉后3 bits; GET_ALLOC, 取最后一位
+- HDRP, 给block pointer强转 1Byte的指针, -4, 相当于-32, 指向block header
+- FTRP: header + get_size - 2B; get_size获得的值可能是任意字节, 所以强转 char*
+- NEXT_BLKP: `(char*)(bp) + GETSIZE((char *)bp - WSIZE) `
+- PREV_BLKP: `(char*)(bp) - GETSIZE(char* bp - DSIZE)`
+
 ### mm.c
 
-核心文件
+#### mm_init
 
-- `mm_init`用于初始化 heap, 如果有问题就返回-1, 否则返回0
+- 返回初始化状态
+- 首先申请4 words的大小, 用于储存prologue and epilogue block, 并且赋值
+- heap_listp指向合适的位置
+- 申请 1 chunk的内存
+
+#### extend_heap
+
+- 理论上, 当前brk的值就是应该返回的bp
+- 接收一个words, 重整为8B对齐的bytes, 然后再mem_sbrk
+- 改变bp对应的header和footer的大小和状态
+- extend后, epilogue block被挤占了, 需要重新赋值; next header处应该赋值
+- coalesce
+
+#### coalesce
+
+- 根据前后block的状态, 修改对应的大小
+- 注意: 如果和prev block合并, 需要修改bp的值
+
+#### mm_free
+
+- 修改header, footer
+- coalesce
+
+#### mm_malloc
+
+- 先做内存对齐
+- 寻找合适的block
+- 如果没有合适的block, 就需要extend
+- 找到一个位置, 并且place
+
+#### find_fit
+
+- 从头开始, 寻找一个free并且空间足够的block
+- 如果指向了epilogue, 就return NULL
+
+#### place
+
+- 放置
+- 如果空闲长度 > 8, 就split
+
+#### mm_realloc
+
+
+
+
+
+
+
 - `mm_malloc(size_t size)`返回一个指针 (指向payload), malloc不应超出heap, 也不应该和已经分配的blocks overlap; **8 bytes对齐 (为了提高内存访问效率, 64位机器内存都是8B对齐的)**
 - `mm_free(void *ptr)`释放, 前提是这个指针必须是通过malloc/realloc获取的
 - `mm_realloc(void* ptr, size_t size)`比较灵活
@@ -38,7 +102,7 @@
   - 是否有allocated blocks overlap
   - heap block是否指向有效的heap address
 
-
+- 
 
 
 
